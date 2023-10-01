78f0d4d261ddc9b20ac901f9afb1179f8b0d3a86
Rockey Kumar <104070946+rockeykumarmuz@users.noreply.github.com>
2023-10-01 22:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+let listContainer = document.getElementById("infi-list");
+let i = 0;
+
+function addList() {
+	let list = document.createElement("li");
+	list.innerText = `Item ${++i}`;
+	listContainer.appendChild(list);
+}
+
+for(let j=0; j<10; j++) {
+	addList();
+}
+
+listContainer.addEventListener("scroll", ()=>{
+	let maxHeight = listContainer.scrollHeight - listContainer.clientHeight;
+	let scrollHeight = listContainer.scrollTop;
+
+	let threshold = Math.abs(maxHeight - scrollHeight);
+
+	if(threshold <= 5) {
+		for(let j=0; j<2; j++) {
+			addList();
+		}
+	}
+})
 
